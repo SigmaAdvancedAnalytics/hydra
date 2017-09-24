74fbf809299b307027bf6b78cbe893f3f0caabff
Josh Barber <j.barber501@gmail.com>
2017-09-23 21:20:29 -0400

    Refactoring code

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='hydra',
+	version='0.1',
+	description='',
+	author='Joshua Barber',
+	url='https://github.com/zejn/lnlib',
+	py_modules=['lnlib'],
+)
--- tableau_connector.py
+++ tableau_connector.py
@@ -16,3 +16,38 @@ cursor = conn.cursor()
 print("Connected!\n")
 
 #KennaG123
+#SQL alchemy connection example
+"""
+import sqlalchemy
+
+SERVER_NAME = ''
+DATABASE_NAME = ''
+
+#Establish windows authenticated session - Not yet tested
+engine = sqlalchemy.create_engine('mssql://'+SERVER_NAME+'/'+DATABASE_NAME+'?trusted_connection=yes')
+"""
+
+# DEPRECATED TSC API CONNECTOR - TO BE USED FOR FILE UPLOADS AT LATER DATE
+"""
+#Tableau Server authentication TODO: convert to .ENV file
+tableau_auth = TSC.TableauAuth('jmayer', 'jmayer')
+tab_server = TSC.Server('https://reports2.agconnect.org/')
+
+def tableau_server_import(tableau_auth, tab_server):
+    try:
+        #Connect to Tableau server
+        with tab_server.auth.sign_in(tableau_auth):
+            workbooks = TSC.Pager(tab_server.workbooks)
+            for wb in workbooks:
+                workbooks_list = workbooks_list.add([wb.project_name,wb.name,wb.content_url,wb.created_at.date(),wb.updated_at.date()])
+                print(workbooks_list)
+        return workbooks_list
+    except:
+        return sys.exc_info()        
+
+
+workbooks = tableau_server_import(tableau_auth, tab_server)
+#for workbook in workbooks:
+ #   print(', '.join(workbook))
+
+"""
